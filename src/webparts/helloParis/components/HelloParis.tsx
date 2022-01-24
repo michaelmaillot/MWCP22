dc264f8b54faacc704f498cb904f522a2da6ce58
Michael MAILLOT <michael.maillot@groupeonepoint.com>
2022-01-24 16:19:00 +0100

    Update

--- src/webparts/helloParis/components/HelloParis.tsx
+++ src/webparts/helloParis/components/HelloParis.tsx
@@ -10,7 +10,7 @@ export default class HelloParis extends React.Component<IHelloParisProps, {}> {
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
+              <span className={ styles.title }>Welcome to the Modern Workplace Conference Paris 2022!</span>
               <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
